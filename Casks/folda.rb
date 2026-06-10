ec88ff7fdc10b87b30370801d52dd925fd6f1b76
Hidekazu Inoue <inoue@yatatechnologies.jp>
2026-06-10 21:20:23 +0900

    folda 0.4.0: host on gmail R2

--- Casks/folda.rb
+++ Casks/folda.rb
@@ -2,7 +2,7 @@ cask "folda" do
   version "0.4.0"
   sha256 "ccece36bb22cd8c77bde3cd6a8a9312fbae584e7af5ddb01a0e48493077e60f9"
 
-  url "https://pub-29ef19c089e742b7bfa42c0c5ac5aba7.r2.dev/Folda-#{version}.dmg"
+  url "https://pub-33dd39159ed040fda36fa3c2635272bd.r2.dev/Folda-#{version}.dmg"
   name "Folda"
   desc "Dual-pane macOS file manager"
   homepage "https://folda.pages.dev/"
